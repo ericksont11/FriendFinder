b995350f78c1cddb38d9ae1ab27aa4e7b2f8ca34
ericksont11 <ericksont91@gmail.com>
2019-03-09 23:25:20 -0500

    added modal and logic for matching

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,36 @@
-const friends = []
+const friends = [
+    {
+    "name": "Kelsey Dale Goodman",
+    "photo": "https://pbs.twimg.com/profile_images/958541560341258240/3dMCn5sS_400x400.jpg",
+    "scores": [
+        "4",
+        "5",
+        "4",
+        "2",
+        "4",
+        "1",
+        "4",
+        "2",
+        "1",
+        "4"
+        ]
+    },
+    {
+    "name": "Thomas Erickson",
+    "photo": "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/1924009_1022517802422_5578_n.jpg?_nc_cat=109&_nc_ht=scontent-iad3-1.xx&oh=1613907adb3702714cfdaa5cec85da5a&oe=5D1078F9",
+    "scores": [
+        "5",
+        "3",
+        "4",
+        "2",
+        "4",
+        "1",
+        "4",
+        "1",
+        "1",
+        "4"
+        ]
+    }
+]
 
 module.exports = friends
