142f96bd7bc53876f457ce93f58743b64f4e04f1
ericksont11 <ericksont91@gmail.com>
2019-03-09 23:39:48 -0500

    added more users to the database

--- app/data/friends.js
+++ app/data/friends.js
@@ -30,6 +30,70 @@ const friends = [
         "1",
         "4"
         ]
+    },
+    {
+    "name": "Bob Ross",
+    "photo": "https://www.wikitree.com/photo.php/0/02/Ross-14816.jpg",
+    "scores": [
+        "5",
+        "1",
+        "5",
+        "1",
+        "5",
+        "1",
+        "3",
+        "5",
+        "3",
+        "3"
+        ]
+    },
+    {
+    "name": "Dwayne Johnson",
+    "photo": "https://usatftw.files.wordpress.com/2014/07/rock.jpg?w=660&h=400&crop=1",
+    "scores": [
+        "5",
+        "3",
+        "5",
+        "1",
+        "1",
+        "1",
+        "3",
+        "1",
+        "3",
+        "5"
+        ]
+    },
+    {
+    "name": "Tywin Lannister",
+    "photo": "https://img.cinemablend.com/filter:scale/cb/8/a/3/d/f/d/8a3dfdcf7c5b958241cd15947900518963c3b4efcb7c4f33c370dcee4ef788f3.jpg?mw=600",
+    "scores": [
+        "2",
+        "5",
+        "5",
+        "5",
+        "1",
+        "3",
+        "5",
+        "1",
+        "1",
+        "2"
+        ]
+    },
+    {
+    "name": "Kate McKinnon",
+    "photo": "https://peopledotcom.files.wordpress.com/2018/03/kate-mckinnon.jpg",
+        "scores": [
+        "5",
+        "4",
+        "5",
+        "2",
+        "5",
+        "1",
+        "3",
+        "1",
+        "1",
+        "5"
+        ]
     }
 ]
 
