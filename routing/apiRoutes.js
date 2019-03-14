696803b9dd1c3426c69266672d3bd38d6ea66bd9
ericksont11 <ericksont91@gmail.com>
2019-03-14 13:47:22 -0400

    added logic to check if all fields were completed

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -3,19 +3,19 @@ module.exports = function(app, express, friends) {
     app.use(express.json());
 
     //API ROUTING GET METHODS
-    app.get("/friends", (req, res)=>{
+    app.get("/api/friends", (req, res)=>{
         return res.json(friends)
     })
    
     //API ROUTING POST METHODS
     app.post("/data/friends", (req, res)=>{
 
-        const newBooking = req.body;
+        const newFriend = req.body;
 
-        console.log(newBooking)
+        console.log(newFriend)
 
-        friends.push(newBooking)
+        friends.push(newFriend)
 
-        res.json(newBooking)
+        res.json(newFriend)
     })
 }
